a221dfec9281796371d15596c2d6d1b4e309ef0f
matias <matias_ortiz_@outlook.es>
2024-04-22 18:57:14 -0300

    add getUserId function

--- src/main/java/com/ecommerce/services/vehicles/CarService.java
+++ src/main/java/com/ecommerce/services/vehicles/CarService.java
@@ -6,12 +6,10 @@ import com.ecommerce.exceptions.UserMismatchException;
 import com.ecommerce.models.Product;
 import com.ecommerce.models.User;
 import com.ecommerce.models.vehicles.Car;
-import com.ecommerce.models.vehicles.CarRequest;
 import com.ecommerce.repositories.ProductRepository;
 import com.ecommerce.repositories.UserRepository;
 import com.ecommerce.repositories.vehicles.CarRepository;
 import com.ecommerce.services.AuthService;
-import com.ecommerce.utils.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,16 +19,14 @@ import java.util.List;
 public class CarService {
     private final CarRepository carRepository;
     private final AuthService authService;
-    private final JWTUtil jwtUtil;
     private final UserRepository userRepository;
     private final ProductRepository productRepository;
 
     @Autowired
-    public CarService(CarRepository carRepository, AuthService authService, JWTUtil jwtUtil, UserRepository userRepository,
+    public CarService(CarRepository carRepository, AuthService authService, UserRepository userRepository,
                        ProductRepository productRepository) {
         this.carRepository = carRepository;
         this.authService = authService;
-        this.jwtUtil = jwtUtil;
         this.userRepository = userRepository;
         this.productRepository = productRepository;
     }
@@ -45,11 +41,11 @@ public class CarService {
 
     public Car addCar(Car car, String token) {
         if(authService.validationToken(token)) {
-            String userId = jwtUtil.getKey(token);
+            Long userId = authService.getUserId(token);
 
             Product newProduct = car.getProduct();
             Product product = productRepository.save(newProduct);
-            User user = userRepository.findById(Long.valueOf(userId)).orElseThrow(() -> new ResourceNotFoundException("The user is not found"));
+            User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("The user is not found"));
 
             car.setProduct(product);
             car.setUser(user);
@@ -60,9 +56,9 @@ public class CarService {
 
     public void deleteCar(Long id, String token) {
         if(authService.validationToken(token)) {
-            String userId = jwtUtil.getKey(token);
+            Long userId = authService.getUserId(token);
             Car car = carRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("The car with this id: " + id + "is not found"));
-            if(car.getUser().getId().equals(Long.valueOf(userId))) {
+            if(car.getUser().getId().equals(userId)) {
                 carRepository.delete(car);
             } throw new UserMismatchException("User mismatch");
         } throw new UnauthorizedException("Unauthorized: invalid token");
